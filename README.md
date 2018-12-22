2090c67ea431be56df384f3a1e2f609a8d558c72
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-12-21 21:51:44 -0800

    Added requirements and start of just python script.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # edX-to-gradescope
 
 A script for converting edX's online assignment format to Gradescope online assignment format.
+
+
+Some notes:
+Also it looks like you do not even need to put in the hashes for the different assignments. I think I figured out the tree to search through the pre-existing structure. You first look at the course.xml to get the url_name, you then go to the course/*url_name*.xml file to read the current homeworks. From there you go into the chapter folder for each chapter hash you have. The chapter hash file gives you the sequential hash file which has a folder containing another file. That file will then have the verticals which you then need.
---
+++ hw_conversion.py
@@ -0,0 +1,283 @@
+import sys
+from pprint import pprint
+import os
+from bs4 import BeautifulSoup
+import re
+import traceback
+from unidecode import unidecode
+import mpmath
+
+# For python code form edX
+import random
+
+path_to_class_data = './course_su18/'
+
+hw0_vertical_names = [
+    '60fb25d0e9f0487dad5d91a1ef501fd9',
+    '908da8a3abda4a019e8e42416ae61555',
+]
+
+def main():
+    make_assignment_from_vertical_names(hw0_vertical_names)
+
+def make_assignment_from_vertical_names(vertical_names):
+    
+    for vertical_name in vertical_names:
+        path_to_vertical = find('{}.xml'.format(vertical_name), path_to_class_data)
+        if path_to_vertical is None:
+            raise Exception('Path to vertical is None!')
+        with open(path_to_vertical) as f:
+            soup = BeautifulSoup(f, 'html.parser')
+        
+        vertical_display_name = soup.find_all('vertical')[0]['display_name']
+        problem_url_names_and_types = [(tag['url_name'], tag.name) for tag in soup.find_all(['problem', 'html'])]
+        for part_index, problem_url_name_and_type in enumerate(problem_url_names_and_types):
+            problem_url_name, problem_url_type = problem_url_name_and_type
+            if problem_url_type == 'problem':
+                path_to_problem = find('{}.xml'.format(problem_url_name), path_to_class_data)
+            elif problem_url_type == 'html':
+                path_to_problem = find('{}.html'.format(problem_url_name), path_to_class_data)
+            else:
+                raise Exception('fuck up')
+            with open(path_to_problem) as f:
+                problem_soup = BeautifulSoup(f, 'html.parser')
+
+            for tag in problem_soup.find_all('solution'):
+                tag.clear()
+
+            script = problem_soup.find(['script'])
+            if script:
+                try:
+                    exec(format_python_code(script.text), globals())
+                except Exception as e:
+                    print(e)
+                    print(traceback.format_exc())
+                    print('ERROR WITH EXECUTING CODE: ', problem_url_name)
+            
+            tic = TableIndexCounter()
+            try:
+                problem_text = convert_tag(problem_soup, tic)
+            except Exception as e:
+                print(e)
+                print(traceback.format_exc())
+                print('ERROR WITH TRANSLATING: ', problem_url_name)
+            
+            if part_index != 0:
+                print('---------------------\n\n')
+                print('Part {}'.format(part_index + 1))
+            print(problem_text)
+        
+        print('\n$$\\phantom{\\rule{0em}{10em}}$$')
+        print('___')
+        print('\n\n\n--------------------------------------------------------------------\n\n\n')
+
+class TableIndexCounter:
+    def __init__(self):
+        self.table_index = 0
+
+class TableEntryProcessor:
+    def __init__(self, tic):
+        self.answers = {}
+        self.nested_tables = {}
+        self.tic = tic
+    
+    def process(self, tag):
+        table = tag.find('table')
+        textline = tag.find('textline')
+        if table:
+            text = self.process_nested_table(table)
+            return text
+        elif textline:
+            if not textline.has_attr('correct_answer'):
+                return ''
+            else:
+                correct_answer_text, correct_answer_value, points = convert_textline_format(textline)
+                self.answers[correct_answer_text] = {
+                    'value': correct_answer_value,
+                    'points': points,
+                }
+                return '**' + correct_answer_text + '**'
+        else:
+            text = re.sub('\$\w+', self.process_match, tag.text)
+            text = convert_latex_format(text.strip())
+            return text
+        
+    def process_nested_table(self, table):
+        self.nested_tables[self.tic.table_index] = convert_table_format(table, self.tic)
+        text = 'Table' + str(self.tic.table_index)
+        self.tic.table_index += 1
+        return text
+        
+    def process_match(self, match):
+        text = '{' + match.group(0)[1:] + '}'
+        text = text.format(**globals())
+        try:
+            # This still doesn't work
+            text = str(float(str(text)))
+        except:
+            pass
+        if '</table>' in text:
+            table = BeautifulSoup(text, 'html.parser').find('table')
+            text = self.process_nested_table(table)
+        return text
+
+def evaluate_variable(match):
+    matched_text = match.group(0)[1:]
+    text = '{' + matched_text + '}'
+    try:
+        text = text.format(**globals())
+    except KeyError:
+        pass
+    return text
+
+
+def evaluate_variables(text):
+    return re.sub('\$\w+', evaluate_variable, text)
+
+
+def convert_latex_format(text):
+    return text.replace('\(', '$$').replace('\)', '$$').replace('\[', '$$').replace('\]', '$$').replace('|', ' \mid ')
+
+
+def convert_img_format(img_src):
+    return '![](https://rawgit.com/AdityaB97/cs188/master{})'.format(evaluate_variables(img_src))
+
+
+def convert_numericalresponse_format(answer):
+    return '[____]({})'.format(evaluate_variables(answer))
+
+
+def convert_choiceresponse_format(tag):
+    result = ''
+    multichoice = sum([choice['correct'] == 'true' for choice in tag.find_all('choice')]) > 1
+    for choice in tag.find_all('choice'):
+        if multichoice:
+            prefix = '[X] ' if choice['correct'] == 'true' else '[ ] '
+        else:
+            prefix = '(X) ' if choice['correct'] == 'true' else '( ) '
+        result += prefix
+        if choice.find('text') is not None:
+            result += choice.find('text').text.strip() + '\n'
+        else:
+            # List comprehension below is a quick fix for HW10 Q6 (actually second question)
+            result += ' '.join([str(thing) for thing in choice.contents]) + '\n'
+    return result
+
+
+def convert_textline_format(textline):
+    assert textline.has_attr('correct_answer')
+    correct_answer_text = re.search('\$\w+', textline['correct_answer'])
+    # If the correct answer is a variable, evaluate it. Else, use the raw value
+    if correct_answer_text:
+        correct_answer_text = correct_answer_text.group(0)[1:]
+    else:
+        correct_answer_text = textline['correct_answer']
+    try:
+        correct_answer_value = float(str(eval(correct_answer_text)))
+    except Exception as e:
+        correct_answer_value = str(eval(correct_answer_text))
+    # points = textline['points']
+    # Uncomment if points is needed
+    points = None
+    # Capitalize Answer to make it look better
+    correct_answer_text = correct_answer_text.capitalize()
+    
+    return correct_answer_text, correct_answer_value, points
+    
+
+def convert_table_format(table, tic):
+    tep = TableEntryProcessor(tic)
+    output_string = ''
+    
+    if table.find('tbody'):
+            table = table.find('tbody')
+            
+    for tag in table.contents:
+        if tag.name == 'tr':
+            # Hacky solution for tables within tables
+            # Removed because this skips tables in variables, like in HW6 Q1
+            # if tag.find('table'):
+            #     continue
+            
+            th_tags = tag.find_all('th', recursive=False)
+            td_tags = tag.find_all('td', recursive=False)
+
+            # Removed temporarily because this fails when there are nested tables
+            # But then resolved the issue by making recursive=False above 
+            assert (len(th_tags) > 0) ^ (len(td_tags) > 0), (th_tags, td_tags)
+
+            if len(th_tags) > 0:
+                output_string += '| ' + ' '.join([tep.process(th_tag) + ' |' for th_tag in th_tags]) + '\n'
+                output_string += '| ' + ' --- |' * len(th_tags) + '\n'
+            elif len(td_tags) > 0:
+                output_string += '| ' + ' '.join([tep.process(td_tag) + ' |' for td_tag in td_tags]) + '\n'
+                
+    output_string += '\n$$\\phantom{\\rule{0em}{1em}}$$'
+    output_string += '\n\n'
+    if len(tep.answers) > 0:
+        output_string += '### Input Answers Here \n'
+    for answer_name, answer_dict in sorted(tep.answers.items(), key=lambda x : x[0]):
+        value, points = answer_dict['value'], answer_dict['points']
+        output_string += '**' + answer_name + ':**' + '\n'
+        output_string += '[____]({})'.format(value) + '\n\n'
+        
+    for index, table in sorted(tep.nested_tables.items(), key=lambda x : x[0]):
+        output_string += '**Table' + str(index) + ':**' + '\n\n'
+        output_string += table + '\n\n'
+        
+    return output_string
+
+def convert_tag(tag, tic):
+    if tag.name == 'table':
+        text = convert_table_format(tag, tic)
+        text += '\n\n'
+        return text
+    elif tag.name is None:
+        tag_text = str(unidecode(tag)).strip()
+        tag_text_evaluated = evaluate_variables(tag_text)
+        if tag_text != tag_text_evaluated:
+            return convert_tag(BeautifulSoup(tag_text_evaluated, 'html.parser'), tic) + '\n\n'
+        else:
+            return convert_latex_format(tag_text) + '\n\n'
+    elif tag.name == 'problem':
+        if tag.has_attr('weight'):
+            weight = tag['weight']
+        else:
+            weight = 1
+        text = tag['display_name'] + ' ' + str(weight)
+        text += '\n\n'
+    elif tag.name == 'img':
+        text = convert_img_format(tag['src'])
+        text += '\n\n'
+    elif tag.name == 'numericalresponse' or tag.name == 'stringresponse':
+        text = convert_numericalresponse_format(tag['answer'])
+        text += '\n\n'
+    elif tag.name == 'choiceresponse' or tag.name == 'multiplechoiceresponse':
+        text = convert_latex_format(convert_choiceresponse_format(tag))
+        text += '\n\n'
+        return text
+    elif tag.name == 'textline':
+        if not tag.has_attr('correct_answer'):
+            text = ''
+        else:
+            correct_answer_text, correct_answer_value, points = convert_textline_format(tag)
+            text = '**' + correct_answer_text + ':**' + '\n'
+            text += '[____]({})'.format(correct_answer_value)
+            text += '\n\n'
+    elif (tag.has_attr('type') and tag['type'] == 'loncapa/python') or tag.name in ['style']:
+        return ''
+    else:
+        text = ''
+        
+    return text + ''.join([convert_tag(child, tic) for child in tag.children if child != '\n'])
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+        
+def format_python_code(text):
+    return text.replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&')
+
+if __name__ == '__main__':
+    main()
